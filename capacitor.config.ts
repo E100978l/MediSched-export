2f7f5f1b7da2c8cd0973ddcf3175503d0f00bd88
E100978l <chrislumbert@icloud.com>
2026-09-12 19:31:11 -0700

    Export source file capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.medisched.app',
+  appName: 'MediSched',
+  webDir: 'dist/public'
+};
+
+export default config;
